c230a734820832a5ab5d6b3845e56ba332c60e00
Ajnas Muhammed <ajnasajs44@gmailcom>
2026-05-22 03:07:58 +0530

    add FeaturesSection & StepsSection component

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -4,11 +4,15 @@ import FAQSection from "../(main)/components/FAQSection";
 import Hero from './components/hero';
 import Footer from '@/components/footer';
 import Navbar from '@/components/Navbar';
+import FeaturesSection from './components/FeaturesSection';
+import StepsSection from './components/StepsSection';
 export default function Home() {
   return (
     <main>
     <Navbar />
     <Hero />
+    <FeaturesSection/>
+    <StepsSection/>
     <TestimonialSection/>
     <PricingSection/>
     <FAQSection/>
