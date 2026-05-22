9bb2168452bd7f9f08b0abaa03cffb01b5dc554a
Arun07-8 <arun08kkv@gmail.com>
2026-05-23 00:29:53 +0530

    Revert merge pull request #5 from arun

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,24 +1,27 @@
-import TestimonialSection from "../(main)/components/TestimonialsSection";
-import PricingSection from "../(main)/components/PricingSection";
+import  TestimonialSection from '../(main)/components/TestimonialsSection'
+import  PricingSection from '../(main)/components/PricingSection'
 import FAQSection from "../(main)/components/FAQSection";
-import FeaturesSection from "./components/FeaturesSection";
-import StepsSection from "./components/StepsSection";
-import IndustriesSection from "./components/IndustriesSection";
-import Footer from "@/components/footer";
-import { Navbar, HeroSection } from "@/components/landing";
+import Hero from './components/hero';
+import Footer from '@/components/footer';
+import Navbar from '@/components/Navbar';
+import FeaturesSection from './components/FeaturesSection';
+import StepsSection from './components/StepsSection';
+import IndustriesSection from './components/IndustriesSection';
 
 export default function Home() {
   return (
     <main>
-      <Navbar />
-      <HeroSection />
-      <FeaturesSection />
-      <StepsSection />
-      <IndustriesSection />
-      <TestimonialSection />
-      <PricingSection />
-      <FAQSection />
-      <Footer />
+    <Navbar />
+    <Hero />
+    <FeaturesSection/>
+    <StepsSection/>
+    <IndustriesSection/>
+    <TestimonialSection/>
+    <PricingSection/>
+    <FAQSection/>
+    <Footer />
     </main>
   );
 }
+
+
