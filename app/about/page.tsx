1f95c9d2fa7754ecb2fcdfd461e474b52f4c6436
Sidhique <xidhique@gmail.com>
2026-05-25 13:11:10 +0530

    Counter

---
+++ app/about/page.tsx
@@ -0,0 +1,14 @@
+import AboutSection from "../(main)/components/AboutSection";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/footer";
+
+export default function AboutPage() {
+  return (
+    <main>
+        <Navbar />
+           
+      <AboutSection />
+      <Footer/>
+    </main>
+  );
+}
